64781e85037471b89daaa346d63b4eb3745703aa
Pia <pia.s.schmidt@googlemail.com>
2019-05-04 15:12:33 +0200

    Testing motion sensor as trigger

--- farbsensor/farbsensor.ino
+++ farbsensor/farbsensor.ino
@@ -2,7 +2,7 @@
   #define ledGreen 3 //colorful laundry
   #define ledBlue 4  //dark laundry
   #define ledWhite 5  //white laundry
-
+  #define motionSensor 6
   #define resetButton 7
 
   #define color_s0 8 //color sensor: output frequencey scaling
@@ -10,15 +10,22 @@
   #define color_s2 10 //color sensor: photodiode type
   #define color_s3 11 //color sensor: photodiode type
   #define colorInput 12 //color sensor: input
-  #define loopDelay 2000 //delay for loop
+  #define loopDelay 500 //delay for loop
   #define logDelay 100  //delay for console log
   
   int frequencyRed = 0;
   int frequencyGreen = 0;
   int frequencyBlue = 0; 
 
+  //counter for laundry
+  int counterColoureds = 0;
+  int counterDark = 0;
+  int counterWhite = 0;
+
   byte error = 0;
   int buttonState = 0;
+
+  int motionDetected = 0;
 
 void setup() {
   //init LEDs
@@ -37,17 +44,15 @@ void setup() {
   // Setting frequency-scaling to 20%   //TODO: testing 100%
   digitalWrite(color_s0,HIGH);
   digitalWrite(color_s1,HIGH);
-
-   digitalWrite(ledRed, HIGH);
-  digitalWrite(ledBlue, HIGH);
-  digitalWrite(ledGreen, HIGH);
-  digitalWrite(ledWhite, HIGH);
-  
+    
   Serial.begin(9600);
 }
 
 void resetCounter(){
-  //TODO reset Counter
+  //reset counter
+  counterColoureds = 0;
+  counterDark = 0;
+  counterWhite = 0;
 
   //turn off any led
   digitalWrite(ledGreen,LOW);
@@ -57,59 +62,93 @@ void resetCounter(){
 }
 
 void loop() {
-
+  //TODO check counter if laundry should be done
+  
   buttonState = digitalRead(resetButton);
-  Serial.println(buttonState);
   if (buttonState == HIGH) {
     resetCounter();
   }
+
+  motionDetected = digitalRead(motionSensor);
+  if (motionDetected == HIGH) {
+    Serial.print("Motion detected");
+
+
+      // Setting red filtered photodiodes to be read
+    digitalWrite(color_s2,LOW);
+    digitalWrite(color_s3,LOW);
+    
+    // Reading the output frequency: length of the pulse (in microseconds)
+    frequencyRed = pulseIn(colorInput, LOW);
+    
+    //Remaping the value of the frequency to the RGB Model of 0 to 255
+    //frequencyRed = map(frequencyRed, 25,72,255,0);                                             //TODO
+    
+    // Printing the value on the serial monitor
+    Serial.print("\n R= ");//printing name
+    Serial.print(frequencyRed);//printing RED color frequency
+    Serial.print("  ");
+    
+    // Setting Green filtered photodiodes to be read
+    digitalWrite(color_s2,HIGH);
+    digitalWrite(color_s3,HIGH);
+    
+    // Reading the output frequency: length of the pulse (in microseconds)
+    frequencyGreen = pulseIn(colorInput,LOW);
+    
+    //Remaping the value of the frequency to the RGB Model of 0 to 255
+    //frequencyGreen = map(frequencyGreen, 30,90,255,0);                                        //TODO
+    
+    // Printing the value on the serial monitor
+    Serial.print("G= ");//printing name
+    Serial.print(frequencyGreen);//printing RED color frequency
+    Serial.print("  ");
+    
+    // Setting Blue filtered photodiodes to be read
+    digitalWrite(color_s2,LOW);
+    digitalWrite(color_s3,HIGH);
+    
+    // Reading the output frequency: length of the pulse (in microseconds)
+    frequencyBlue = pulseIn(colorInput,LOW);
+    
+    //Remaping the value of the frequency to the RGB Model of 0 to 255
+    //frequencyBlue = map(frequencyBlue, 25,70,255,0);                      //TODO
+    
+    // Printing the value on the serial monitor
+    Serial.print("B= ");//printing name
+    Serial.print(frequencyBlue);//printing RED color frequency
+    Serial.println("  ");
+  /*
+   * if(frequencyRed >= 200 && frequencyBlue >= 200 && frequencyGreen >= 200) {
+        Serial.println("Weiß erkannt\n");
+        counterWhite++;
+        }
+        else if(frequencyRed >= 200) {
+          Serial.println("Rot erkannt\n");
+          counterColoureds++;
+          
+        }
+         else if(frequencyGreen >= 200) {
+          Serial.println("Grün erkannt\n");
+          counterColoureds++;
+        }
+         else if(frequencyBlue >= 200) {
+          Serial.println("Blau erkannt\n");
+           counterColoureds++;
+        }
+         else if(frequencyRed <= -200 && frequencyBlue <= -200 && frequencyGreen <= -200) {
+          Serial.println("Schwarz erkannt\n");
+          counterDark++;
+      } else {
+        Serial.println("Messung war ungenau");
+      }
+   */
+        
+  }else{
+    Serial.println("NO Motion detected");
+  }
  
-  // Setting red filtered photodiodes to be read
-  digitalWrite(color_s2,LOW);
-  digitalWrite(color_s3,LOW);
   
-  // Reading the output frequency: length of the pulse (in microseconds)
-  frequencyRed = pulseIn(colorInput, LOW);
-  
-  //Remaping the value of the frequency to the RGB Model of 0 to 255
-  //frequencyRed = map(frequencyRed, 25,72,255,0);                                             //TODO
-  
-  // Printing the value on the serial monitor
-  Serial.print("R= ");//printing name
-  Serial.print(frequencyRed);//printing RED color frequency
-  Serial.print("  ");
-  delay(logDelay);
-  
-  // Setting Green filtered photodiodes to be read
-  digitalWrite(color_s2,HIGH);
-  digitalWrite(color_s3,HIGH);
-  
-  // Reading the output frequency: length of the pulse (in microseconds)
-  frequencyGreen = pulseIn(colorInput,LOW);
-  
-  //Remaping the value of the frequency to the RGB Model of 0 to 255
-  //frequencyGreen = map(frequencyGreen, 30,90,255,0);                                        //TODO
-  
-  // Printing the value on the serial monitor
-  Serial.print("G= ");//printing name
-  Serial.print(frequencyGreen);//printing RED color frequency
-  Serial.print("  ");
-  delay(logDelay);
-  
-  // Setting Blue filtered photodiodes to be read
-  digitalWrite(color_s2,LOW);
-  digitalWrite(color_s3,HIGH);
-  
-  // Reading the output frequency: length of the pulse (in microseconds)
-  frequencyBlue = pulseIn(colorInput,LOW);
-  
-  //Remaping the value of the frequency to the RGB Model of 0 to 255
-  //frequencyBlue = map(frequencyBlue, 25,70,255,0);                      //TODO
-  
-  // Printing the value on the serial monitor
-  Serial.print("B= ");//printing name
-  Serial.print(frequencyBlue);//printing RED color frequency
-  Serial.println("  ");
-  delay(loopDelay);
+  delay(loopDelay);       //the button needs to be pressed longer than loopDelay-> should not be too long (>2 sec)
   
 }
