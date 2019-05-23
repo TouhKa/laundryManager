7353ea0a9a7562e8987d442ddfe293ea653c50d1
Pia <pia.s.schmidt@googlemail.com>
2019-05-23 09:53:50 +0200

    lcd display added, cleaned up comments

--- farbsensor/farbsensor.ino
+++ farbsensor/farbsensor.ino
@@ -1,3 +1,5 @@
+  #include <LiquidCrystal.h>
+  
   #define ledRed 2 //blinking red led for errors
   #define ledGreen 3 //colorful laundry
   #define ledBlue 4  //dark laundry
@@ -10,8 +12,16 @@
   #define color_s2 10 //color sensor: photodiode type
   #define color_s3 11 //color sensor: photodiode type
   #define colorInput 12 //color sensor: input
-  #define loopDelay 100 //delay for loop
-  #define logDelay 100  //delay for console log
+  
+  #define loopDelay 500 //delay for loop
+
+  //lcd display
+  #define rs 52
+  #define en 50
+  #define d4 40
+  #define d5 36
+  #define d6 32
+  #define d7 28
 
   int loopCount = 0;
   int val = 0;    // temp value for calculationg the average of 3 measurements
@@ -27,79 +37,112 @@
 
   byte error = 0;
   int buttonState = 0;
-
   int motionDetected = 0;
 
+  LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+/*
+ * init LEDS, color sensor and LCD display.
+ * Set the frequency scaling of the color sensor to 100% (HIGH, HIGH)
+ * Show 30 characters and 2 lines on the display
+ * Set baud rate
+ */
 void setup() {
-  //init LEDs
   pinMode(ledRed, OUTPUT);
   pinMode(ledBlue, OUTPUT);
   pinMode(ledGreen, OUTPUT);
   pinMode(ledWhite, OUTPUT);
 
-  // init color sensor
   pinMode(color_s0, OUTPUT);
   pinMode(color_s1, OUTPUT);
   pinMode(color_s2, OUTPUT);
   pinMode(color_s3, OUTPUT);
   pinMode(colorInput, INPUT);
-  
-  // Setting frequency-scaling to 20%   //TODO: testing 100%
+
+  //100%
   digitalWrite(color_s0,HIGH);
   digitalWrite(color_s1,HIGH);
-    
+
+  lcd.begin(30, 2);
+  lcd.display();
+
   Serial.begin(9600);
 }
 
+
+/*
+ * Button handles the color counters and any error messages
+ * @Param flag indicates whicht type of event neeeds to be handled
+ * flag == 0 -> turn of error message
+ * flag == 1 -> reset counter and turn of leds
+ */
 void buttonPressed(byte flag){
 
-  if (flag == 0){     //flag == 0 -> turn of error message
-    
-   //reset error flag
+  if (flag == 0){     
     error = 0;
     digitalWrite(ledRed,LOW);
     
-  }else {            //flag == 1 -> reset counter
-   
+  }else {            
     counterColoureds = 0;
     counterDark = 0;
     counterWhite = 0;
-    //turn off any led
     digitalWrite(ledGreen,LOW);
     digitalWrite(ledBlue,LOW);
     digitalWrite(ledWhite,LOW);
   }
 }
 
-
+/*
+ * If there are undefined measurments set error flag and turn on red led
+ */
 void sendErrorWarning(int flag) {
     if (flag == 0){
-      Serial.print("Ungenaue Messung");
+      lcd.println("Ungenaue Messung");
       error = 1;
       digitalWrite(ledRed,HIGH);
     }
  }
 
+/*
+ * Evaluates the measurement and sums up the right color counter
+ * prints out the color on the display
+ * @param average of 3 measurements
+ * if (val == 3|| val == 22 || val == 12) -> black
+ * if(val == 41 || val == 50|| val == 60 ) -> white
+ * if (val == 31) -> undefined color due to three different measurements
+ * else -> color
+ */
  void getResult(int val){
+    Serial.println(val);
    
-    if (val == 3|| val == 22 || val == 12){       //black
+    if (val == 3|| val == 22 || val == 12){       
       Serial.println("Schwarz erkannt\n");
+      lcd.println("Schwarz");
       counterDark++;
 
-    }else if(val == 41 || val == 50|| val == 60 ){    //white
+    }else if(val == 41 || val == 50|| val == 60 ){    
       Serial.println("Weiß erkannt\n");
+      lcd.println("Weiß");
       counterWhite++; 
     
-    }else if(val == 31){                           //undefined colour
+    }else if(val == 31){                           
       //turn on red led to signal an error
       sendErrorWarning(0);
 
-    }else{                                  //color
+    }else{                                  
        Serial.println("Bunt");
+       lcd.println("Bunt");
        counterColoureds++;
     }
+    lcd.clear();
  }
-
+/*
+ * Main loop. 
+ * Handles button input depending if error flag is set
+ * Reading the output frequency: length of the pulse (in microseconds)
+ * Remaping the value of the frequency to the RGB Model of 0 to 255 (first red, then green and blue)
+ * Summ up the weighted results (white -> + 20, black + 1, color -> + 10)and evaluate the average
+ */
 void loop() {
   //TODO check counter if laundry should be done
   
@@ -115,69 +158,45 @@ void loop() {
   if (error != 1){
 
     if (loopCount < 3){
-        // Setting red filtered photodiodes to be read
+    
         digitalWrite(color_s2,LOW);
         digitalWrite(color_s3,LOW);
-        
-        // Reading the output frequency: length of the pulse (in microseconds)
         frequencyRed = pulseIn(colorInput, LOW);
-        
-        //Remaping the value of the frequency to the RGB Model of 0 to 255
-        frequencyRed = map(frequencyRed, 7,57,255,0);  
-        
-        
-        // Printing the value on the serial monitor
-        Serial.print("\n R= ");//printing name
-        Serial.print(frequencyRed);//printing RED color frequency
+        frequencyRed = map(frequencyRed, 7,60,255,0);  
+        Serial.print("\n R= ");
+        Serial.print(frequencyRed);
         Serial.print("  ");
-        
-        // Setting Green filtered photodiodes to be read
+
         digitalWrite(color_s2,HIGH);
         digitalWrite(color_s3,HIGH);
-        
-        // Reading the output frequency: length of the pulse (in microseconds)
         frequencyGreen = pulseIn(colorInput,LOW);
-        
-        //Remaping the value of the frequency to the RGB Model of 0 to 255
-        frequencyGreen = map(frequencyGreen, 2,66,255,0);                                     
-        
-        // Printing the value on the serial monitor
-        Serial.print("G= ");//printing name
-        Serial.print(frequencyGreen);//printing RED color frequency
+        frequencyGreen = map(frequencyGreen, 2,75,255,0); 
+        Serial.print("G= ");
+        Serial.print(frequencyGreen);
         Serial.print("  ");
-        
-        // Setting Blue filtered photodiodes to be read
+
         digitalWrite(color_s2,LOW);
         digitalWrite(color_s3,HIGH);
-        
-        // Reading the output frequency: length of the pulse (in microseconds)
         frequencyBlue = pulseIn(colorInput,LOW);
-        
-        //Remaping the value of the frequency to the RGB Model of 0 to 255
-        frequencyBlue = map(frequencyBlue, 2,58,255,0);                      
-        
-        // Printing the value on the serial monitor
-        Serial.print("B= ");//printing name
-        Serial.print(frequencyBlue);//printing RED color frequency
+        frequencyBlue = map(frequencyBlue, 2,70,255,0);
+        Serial.print("B= ");
+        Serial.print(frequencyBlue);
         Serial.println("  ");
     
        if(frequencyRed >= 200 && frequencyBlue >= 150 && frequencyGreen >= 150) {
           val += 20;
-          
        }else if(frequencyRed <= 150 && frequencyBlue <= 150 && frequencyGreen <= 150) {
          val += 1;
-         
       } else {
         val += 10;
       }
-      loopCount ++;
+      loopCount++;
           
    }else{
       getResult(val);
       loopCount = 0;
+      val = 0;
     }
   }
-
-  delay(loopDelay);       //the button needs to be pressed longer than loopDelay-> should not be too long (>2 sec)
-  
+  delay(loopDelay);   
 }
